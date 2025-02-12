46b9e60f8786f8b59822cadbcec84d509d86b367
Karadumk <karadumk223@gmail.com>
2025-02-12 11:53:36 +0100

    test: testing CD pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -80,7 +80,7 @@ async def update_book(book_id: int, book: Book) -> Book:
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
-
+# running test
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
